e7dc6ff64a913603181f20bd0aded3857c69b81d
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-09-29 16:16:00 -0400

    added comments

--- jenny-dropdownmenu.js
+++ jenny-dropdownmenu.js
@@ -1,5 +1,7 @@
+// get element for selecting color
 let selectColor = document.body.querySelector("#select-color");
 
+// when the value is chosen on click, the color value will be collected
 selectColor.addEventListener("click", () => {
     console.log(selectColor.value);
 });
